6a037901b6a6f9f42c84fd369053387574100568
Narendra kumar <sharmanarendar598@gmail.com>
2026-06-19 10:31:30 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/https://github.com/sharmanarendar598-star/Narendramall.git/', 
-  base:'/jghjvhdf/',
+  base: '/Narendra-mall/', 
   plugins: [react()],
 })
